38c9584358b610200256ae8477173dae00932466
Non <natthanonprajogo@gmail.com>
2025-10-11 18:14:51 +0700

    finished sidebar

--- animal-shelter-manager/src/lib/components/SideBar/SideBar.svelte
+++ animal-shelter-manager/src/lib/components/SideBar/SideBar.svelte
@@ -6,6 +6,7 @@ This file defines a reusable SideBar component.
 
 <script lang="ts">
   import "./style.scss";
+  import { User } from "@lucide/svelte";
 
   // Fields
   export let username: string = "Username";
@@ -40,20 +41,7 @@ This file defines a reusable SideBar component.
 <div class="sidebar">
   <div class="user-profile">
     <div class="user-icon">
-      <svg
-        width="30"
-        height="30"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12Z"
-        />
-        <path
-          d="M12 14C6.47715 14 2 16.4772 2 19.5V22H22V19.5C22 16.4772 17.5228 14 12 14Z"
-        />
-      </svg>
+      <User size="56" class="user-icon" />
     </div>
     <div class="user-info">
       <h2>{username}</h2>
