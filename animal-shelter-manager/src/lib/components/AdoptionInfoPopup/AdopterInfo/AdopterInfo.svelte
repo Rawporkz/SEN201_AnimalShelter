0f1b962d984340d08fa2c3b937a10762064095cc
Non <natthanonprajogo@gmail.com>
2025-10-12 17:13:40 +0700

    Added the close button, removed unecessary overlay

--- animal-shelter-manager/src/lib/components/AdoptionInfoPopup/AdopterInfo/AdopterInfo.svelte
+++ animal-shelter-manager/src/lib/components/AdoptionInfoPopup/AdopterInfo/AdopterInfo.svelte
@@ -5,8 +5,8 @@ This file defines a reusable AdopterInfo component.
 -->
 <script lang="ts">
   import "./style.scss";
-  import { User } from "@lucide/svelte";
   import { emit } from "@tauri-apps/api/event";
+  import ClosePopupButton from "../../ClosePopupButton/ClosePopupButton.svelte";
 
   // Fields
   export let name: string = "Adopter Name";
@@ -25,64 +25,65 @@ This file defines a reusable AdopterInfo component.
   };
 </script>
 
-<div class="adopter-info-overlay">
-  <div
-    class="adopter-info-modal"
-    role="dialog"
-    aria-modal="true"
-    tabindex="0"
-    on:keydown={(e) => {
-      if (e.key === "Escape") closeModal();
-    }}
-  >
-    <div class="adopter-info-header">
-      <h1>Adopter Information</h1>
+<div
+  class="adopter-info-modal"
+  role="dialog"
+  aria-modal="true"
+  tabindex="0"
+  on:keydown={(e) => {
+    if (e.key === "Escape") closeModal();
+  }}
+>
+  <div class="close-button-wrapper">
+    <ClosePopupButton on:click={closeModal} />
+  </div>
+  <div class="adopter-info-header">
+    <h1>Adopter Information</h1>
+  </div>
+  <div class="adopter-info-content">
+    <div class="adopter-info-row">
+      <div class="adopter-info-item">
+        <div class="label">Name</div>
+        <div class="value">{name}</div>
+      </div>
+      <div class="adopter-info-item">
+        <div class="label">Occupation</div>
+        <div class="value">{occupation}</div>
+      </div>
+      <div class="adopter-info-item">
+        <div class="label">Annual Income</div>
+        <div class="value">{annualIncome}</div>
+      </div>
     </div>
-    <div class="adopter-info-content">
-      <div class="adopter-info-row">
-        <div class="adopter-info-item">
-          <div class="label">Name</div>
-          <div class="value">{name}</div>
-        </div>
-        <div class="adopter-info-item">
-          <div class="label">Occupation</div>
-          <div class="value">{occupation}</div>
-        </div>
-        <div class="adopter-info-item">
-          <div class="label">Annual Income</div>
-          <div class="value">{annualIncome}</div>
-        </div>
+    <div class="adopter-info-row">
+      <div class="adopter-info-item">
+        <div class="label">Email address</div>
+        <div class="value">{email}</div>
       </div>
-      <div class="adopter-info-row">
-        <div class="adopter-info-item">
-          <div class="label">Email address</div>
-          <div class="value">{email}</div>
-        </div>
-        <div class="adopter-info-item">
-          <div class="label">Telephone Number</div>
-          <div class="value">{phoneNumber}</div>
-        </div>
+      <div class="adopter-info-item">
+        <div class="label">Telephone Number</div>
+        <div class="value">{phoneNumber}</div>
       </div>
-      <div class="divider"></div>
-      <div class="adopter-info-item full-width">
-        <div class="label">Street Address</div>
-        <div class="value">{streetAddress}</div>
+    </div>
+    <div class="divider"></div>
+    <div class="adopter-info-item full-width">
+      <div class="label">Street Address</div>
+      <div class="value">{streetAddress}</div>
+    </div>
+    <div class="adopter-info-item full-width">
+      <div class="label">Country</div>
+      <div class="value">{country}</div>
+    </div>
+    <div class="divider"></div>
+    <div class="adopter-info-item full-width">
+      <div class="label">How many people do you have in your household?</div>
+      <div class="value">Answer: {numOfHousehold}</div>
+    </div>
+    <div class="adopter-info-item full-width">
+      <div class="label">
+        Of which, how many are children under 15 years old?
       </div>
-      <div class="adopter-info-item full-width">
-        <div class="label">Country</div>
-        <div class="value">{country}</div>
-      </div>
-      <div class="divider"></div>
-      <div class="adopter-info-item full-width">
-        <div class="label">How many people do you have in your household?</div>
-        <div class="value">Answer: {numOfHousehold}</div>
-      </div>
-      <div class="adopter-info-item full-width">
-        <div class="label">
-          Of which, how many are children under 15 years old?
-        </div>
-        <div class="value">Answer: {numOfChildren}</div>
-      </div>
+      <div class="value">Answer: {numOfChildren}</div>
     </div>
   </div>
 </div>
